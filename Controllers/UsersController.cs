a46285acdb6cb95e29249d6688caa381a8a0e378
MortenStaunstrup <mort490fgromling@gmail.com>
2026-02-24 12:08:48 +0100

    Created repository and endpoint for making login request

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using UserAPI.Models;
 using UserAPI.Repository;
+using UserAPI.Repository;
 // test
 namespace UserAPI.Controllers;
 
@@ -8,9 +9,29 @@ namespace UserAPI.Controllers;
 [Route("[controller]")]
 public class UsersController
 {
+    
+    private readonly IUserRepository _userRepository;
+    
+    public UsersController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
     [HttpPost(Name = "RegisterUser")]
     public  User RegisterUser(User user)
     {
         throw new InvalidOperationException();
     }
+
+    [HttpPost]
+    [Route("users/login")]
+    public IResult Login(UserLogin loginRequest)
+    {
+        User? userExist = _userRepository.Login(loginRequest.username, loginRequest.password);
+        if (userExist == null)
+            return Results.Unauthorized();
+        return Results.Ok(userExist);
+    }
 }
+
+public record UserLogin(string username, string password);
--- Repository/IUserRepository.cs
+++ Repository/IUserRepository.cs
@@ -1,6 +1,8 @@
+using UserAPI.Models;
+
 namespace UserAPI.Repository;
 
 public interface IUserRepository
 {
-    //
+    public User? Login(string username, string password);
 }
